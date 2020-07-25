8a0af5d6e4d0877f9db6f7db388a78ff01ec8fd2
Pinita Promthep <sasuke_kub@hotmail.com>
2020-07-25 17:58:07 +0700

    install gem annotate

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  finished    :boolean
+#  title       :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Todo < ApplicationRecord
 end
